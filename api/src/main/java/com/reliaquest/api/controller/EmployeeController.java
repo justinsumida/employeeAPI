2d8090f00bfdf7ea49a84c8e60e9ee6ea4733007
Justin Sumida <106105694+justinsumida@users.noreply.github.com>
2025-02-22 11:46:22 -0700

    reuse responses

--- api/src/main/java/com/reliaquest/api/controller/EmployeeController.java
+++ api/src/main/java/com/reliaquest/api/controller/EmployeeController.java
@@ -19,73 +19,50 @@ public class EmployeeController implements IEmployeeController<Employee, Employe
     @Override
     @GetMapping("/getAllEmployees")
     public ResponseEntity<List<Employee>> getAllEmployees() {
-        List<Employee> employees = employeeService.getAllEmployees();
-        if (employees == null) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return new ResponseEntity<>(employees, HttpStatus.OK);
+        return response(employeeService.getAllEmployees());
     }
 
     @Override
     @GetMapping("/getEmployeesByName/{searchString}")
     public ResponseEntity<List<Employee>> getEmployeesByNameSearch(@PathVariable String searchString) {
-        List<Employee> employees = employeeService.getEmployeesByName(searchString);
-        if (employees == null) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return new ResponseEntity<>(employees, HttpStatus.OK);
+        return response(employeeService.getEmployeesByName(searchString));
     }
 
     @Override
     @GetMapping("/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable String id) {
-        Employee employee = employeeService.getEmployeeById(id);
-        if (employee == null) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return new ResponseEntity<>(employee, HttpStatus.OK);
+        return response(employeeService.getEmployeeById(id));
     }
 
-    //TODO: unit test this by getting entire list, and then validating that the highest is indeed the highest here.
     @Override
     @GetMapping("/getHighestSalary")
     public ResponseEntity<Integer> getHighestSalaryOfEmployees() {
-        Integer highest = employeeService.getHighestSalary();
-        if (highest == null) { //todo: flip the conditions
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return new ResponseEntity<>(highest, HttpStatus.OK);
+        return response(employeeService.getHighestSalary());
     }
 
     @Override
     @GetMapping("/getTopTenHighestEarningEmployees")
     public ResponseEntity<List<String>> getTopTenHighestEarningEmployeeNames() {
-        List<String> names = employeeService.getTopTenHighestEarners();
-        if (names == null) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-
-        }
-        return new ResponseEntity<>(names, HttpStatus.OK);
+        return response(employeeService.getTopTenHighestEarners());
     }
 
     @Override
     @PostMapping
     public ResponseEntity<Employee> createEmployee(@RequestBody EmployeeInput employeeInput) {
-        Employee newEmployee = employeeService.createEmployee(employeeInput);
-        if (newEmployee == null) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return new ResponseEntity<>(newEmployee, HttpStatus.OK);
+        return response(employeeService.createEmployee(employeeInput));
     }
 
     @Override
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteEmployeeById(@PathVariable String id) {
-        String employeeNameDeleted = employeeService.deleteById(id);
-        if (employeeNameDeleted == null) {
+        return response(employeeService.deleteById(id));
+    }
+
+    private <T> ResponseEntity<T> response(T response) {
+        if (response == null) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>(employeeNameDeleted, HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
 
